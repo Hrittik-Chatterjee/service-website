dcb5edf5102e130b3670301c43d2fbbb5f7ee527
Hrittik-Chatterjee <@hrittik0chatterjee@gmail.com>
2022-11-11 23:23:33 +0600

    blog done

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -9,7 +9,7 @@ const Blog = () => {
                 Difference between SQL and NoSQL
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>SQL is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
                 </div>
             </div>
             <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
@@ -17,7 +17,7 @@ const Blog = () => {
                 What is JWT, and how does it work?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP</p>
                 </div>
             </div>
             <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
@@ -25,7 +25,7 @@ const Blog = () => {
                 What is the difference between javascript and NodeJS?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.</p>
                 </div>
             </div>
             <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
@@ -33,7 +33,7 @@ const Blog = () => {
                 How does NodeJS handle multiple requests at the same time?
                 </div>
                 <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them</p>
                 </div>
             </div>
         </div>
